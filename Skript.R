18684e8265e4f6546580e0bc55583e0f7d2bfcf4
Tingting <tingting.feng@ntnu.no>
2020-12-18 17:37:22 +0100

    Justering skript, slette de som erunødvendig

--- Skript.R
+++ Skript.R
@@ -9,30 +9,14 @@ pacman::p_load(readxl, reshape2,dplyr, GGally, ggplot2, ggthemes,
 data <- read_excel("Covid.xlsx")
 head(data )
 summary(data ) 
-
-
- data <- data %>% 
-  mutate (Måned = factor(dplyr::case_when(
-    month==1 ~ "januar",
-    month==2 ~ "februar",
-    month==3 ~ "mars",
-    month==4 ~ "april",
-    month==5 ~ "mai",
-    month==6 ~ "juni",
-    month==7 ~ "juli",
-    month==8 ~ "august",
-    month==9 ~ "september",
-    month==10 ~ "oktober",
-    month==11 ~ "november",
-    month==12 ~ "desember") )) %>% 
-    rename(År=year)
+str(data)
 
 
 # totalt antall tilfeller og maksimale tilfeller og død etter kontinenter
 antall <- data %>% 
-  rename(Kontinent=continentExp) %>% 
+  rename(Kontinent=continentExp,År = year, Måned= month) %>% 
   group_by(År,Måned,Kontinent,) %>% 
-  summarise(tilfeller_sum = sum(cases), tilfeller_max = max(cases),død_sum = sum(deaths), død_max = max(deaths)) %>% 
+  summarise(tilfeller_sum = sum(cases), tilfeller_mean = mean(cases), tilfeller_max = max(cases),død_sum = sum(deaths), død_mean = mean(deaths),død_max = max(deaths)) %>% 
   arrange(År,Måned)
 
 # ekspotere resultat til excel-format
@@ -60,29 +44,26 @@ us <- data %>%
 
 # plot Norge og US
 
-
 USplot <- ggplot(us, 
                  aes(date, as.numeric(CumulativeNumber))) +
   geom_line(color="blue") + 
-  theme_minimal(base_size = 14) +
-  xlab(NULL) + ylab(NULL) + 
-  scale_x_date(date_labels = "%Y/%m/%d")
-PlotNorUS <- USplot + geom_line(data=Norge, 
+  theme_minimal(base_size = 14)+ xlab(NULL) + ylab(NULL) 
+  
+Norge_US <- USplot + geom_line(data=Norge, 
                                aes(date, as.numeric(CumulativeNumber)),
-                               color="deeppink",
-                               alpha = 0.5) + 
-                      geom_line(data=spain, 
-                              aes(date, as.numeric(CumulativeNumber)),
-                              color="darkorange",
-                              alpha = 0.4)+
-                      geom_line(data=italy, 
-                                aes(date, as.numeric(CumulativeNumber)),
-                                color="#009E73",
-                              alpha = 0.9) + 
-                     ylab("Kumulativ antall for 14 dager per 100,000 innbyggere")+
-                     labs(title="Norge (rosa), US (blå), Italy(grønn), & Spain (gul)")
-jpeg('PlotNorUS.jpg')
-plot(PlotNorUS)
-dev.off()
+                               color="red",
+                               alpha = 0.5)
+Nor_US_Sp <- Norge_US + geom_line(data=spain, 
+                                  aes(date, as.numeric(CumulativeNumber)),
+                                  color="#E69F00",
+                                  alpha = 0.4)
+PlotTrend <- Nor_US_Sp + geom_line(data=italy, 
+                                      aes(date, as.numeric(CumulativeNumber)),
+                                      color="#009E73",
+                                      alpha = 0.9) + 
+                                   labs(title="Norge (ræd), US (blå), Italy(grønn), & Spain (gul)")+ 
+                                   ylab("Kumulativ antall for 14 dager per 100,000 innbyggere") 
+
+ggsave("PlotTrend.png")
 
 
